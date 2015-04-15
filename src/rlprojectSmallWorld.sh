de75277ae78f4fc7fce9d2ecbe6aaf599d4177aa
vgtomahawk <vgtomahawk@gmail.com>
2015-04-15 21:13:45 +0530

    deleted:    Runner.py~

--- src/rlprojectSmallWorld.sh
+++ src/rlprojectSmallWorld.sh
@@ -1,1 +1,1 @@
-python main.py 5 5 100 IntraOptionQ ArbitraryNavigationOptions:5:5:small-world:20:2 testfileSmallWorld
+python main.py 5 5 100 IntraOptionQ ArbitraryNavigationOptions:5:5:2-small-world:10:2 testfile2SmallWorld
